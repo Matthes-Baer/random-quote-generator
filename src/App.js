ccae8be5dbf63b8cd11cc31ad906030cf7ba5083
Matthes-Baer <Matthes.baer@yahoo.de>
2022-04-13 16:21:46 +0200

    cleaned up

--- src/App.js
+++ src/App.js
@@ -6,24 +6,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell } from '@fortawesome/free-solid-svg-icons'
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 
-
-
+// Zugriff auf Quotes
 let quoteDBurl = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
-
 
 function App() {
   /* hiermit kann auf den State zugegriffen werden */
   const [quote, setQuote] = useState("Our lives begin to end the day we become silent about things that matter.");
   const [author, setAuthor] = useState('Martin Luther King Jr.');
-
-  const [randomNumber, setRandomNumber] = useState(0);
-
   const [quotesArray, setQuotesArray] = useState(null);
-
-
-
   const [accentColor, setAccentColor] = useState('#FF6633');
-
 
   const fetchQuotes = async (url) => {
     const response = await fetch(url);
@@ -35,12 +26,6 @@ function App() {
     fetchQuotes(quoteDBurl);
   }, [quoteDBurl])
 
-
-
-
-
-
-
   const getRandomQuote = () => {
     let randomInteger = Math.floor(Math.random() * quotesArray.length);
     setRandomNumber(randomInteger);
@@ -49,8 +34,6 @@ function App() {
     setAccentColor(COLORS_ARRAY[randomInteger]);
   }
   
-
-
   return (
     <div className="App">
       <header className="App-header" style={ {backgroundColor: accentColor} }>
@@ -62,17 +45,13 @@ function App() {
             - {author}
           </p>
         <div className='button'>
-
-        
           <a style={ {backgroundColor: accentColor} } 
           id="tweet-quote" 
           href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote} -${author}`)}
           ><FontAwesomeIcon icon={faBell} /></a>
-          
           <button style={ {backgroundColor: accentColor} } id="new-quote" onClick={() => getRandomQuote()}>
             Generate a random Quote
           </button>
-
           </div>
         </div>
       </header>
